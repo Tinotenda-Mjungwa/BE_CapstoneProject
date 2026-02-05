2c53f70662efd18281c1fab69f7dbbf9af2d00b3
Tinotenda Mjungwa <kimberlymjungwa@gmail.com>
2026-02-05 20:13:08 +0200

    Fix DATABASES config for Render

--- venue_booking_api/venue_booking_api/settings.py
+++ venue_booking_api/venue_booking_api/settings.py
@@ -80,21 +80,14 @@ WSGI_APPLICATION = 'venue_booking_api.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 
+DATABASES = {
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 
-
-DATABASE_URL = os.environ.get("DATABASE_URL")
-
-if DATABASE_URL:
-    DATABASES = {
-        "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=True)
-    }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
 
 
 # Password validation
